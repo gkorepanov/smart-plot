eb593785ab5a36abff85100c8ce46aa438b4c0ae
George Korepanov <gkorepanov.gk@gmail.com>
2017-03-05 04:03:54 +0300

    Started to rewrite everything

--- smartplot.py
+++ smartplot.py
@@ -1,41 +1,136 @@
-import matplotlib.pylab as _plt
+import matplotlib.pylab as plt
 from   matplotlib import rc
+
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+
 import warnings
 from   IPython.display import display
+import pickle
 
 
-# Options
-params = {
-    'text.usetex'         : True,
-    'text.latex.unicode'  : True,
-    'text.latex.preamble' : r"\usepackage[T2A]{fontenc}",
-    'font.size'           : 20,
-    'font.family'         : 'lmodern',
-    'figure.figsize'      : (16, 9),
+class Profile:
+    """
+    input = 'data.csv'
+
+    Matplotlib inherited parameters
+    scatter  =  dict(marker='o', color='r', markersize=7)
+    linear   =  dict(linestyle='--', linewidth=0.5, color='k')
+    grid     =  dict(linestyle='--', linewidth=0.2, color='#e2e2e2')
+
+    Label
+    label = {
+        label: TODO,
+        x: 0.05,
+        y: 0.95,
+        transform: TODO,
+        bbox: TODO
     }
 
-_plt.rcParams.update(params)
+
+    Options
+    rcParams = {
+        'text.usetex': True,
+        'text.latex.unicode': True,
+        'text.latex.preamble': r"\usepackage[T2A]{fontenc}",
+        'font.family': 'lmodern'
+        'font.size': 20,
+        'figure.figsize': (16, 9)
+    }
+    """
+
+    __standard = ['gk']
+
+    def __init__(self, preset='gk', bare=False):
+        if not bare:
+            self.load(preset)
+
+    def load(self, preset):
+        with open(preset + '.profile', 'rb') as file:
+            self.__dict__.update(pickle.load(file))
+
+    def save(self, filename, force=False):
+        """filename of preset to be created or overrided,
+        by default standard presets are not overrided"""
+
+        if preset not in __standard or force:
+            with open(filename, 'wb') as file:
+                pickle.dump(self.__dict__, file)
+        else:
+            raise ValueError("Trying to override the standard preset")
 
 
-def addplot(
-        input  = "data.csv",
-        units  = "",
-        label  = None,
-        labelx = 0.05,
-        labely = 0.9,
-        xerr   = None,
-        yerr   = None,
-        number = 1
+smp.add(SubPlot(fsffsd))
+
+class SmartPlot:
+    class SubPlot:
+        def __init__(
+                self,
+                data=profile.input,
+                x=0, y=1,
+                xerr=None,
+                yerr=None,
+                label=None, units='',
+                settings=None
         ):
+            """
+            SubPlot(data='1', 0, 1, marker={'markersize': 3})
+            SubPlot(data='2', marker={'marker' : 'x', 'color' : 'b'}, linear={'linewidth'=2})
+            """
 
-    # Iterative wrapper
-    if number:
-        # Initialize attributes (static)
-        addplot._row  = 0
-        addplot._data = pd.read_csv(input, engine='python', header=None)
+
+    def __init__(self):
+        self.apply(Profile())
+        self.__subplots = []
+
+    def __getitem__(self, index):
+        return __subplots[index]
+
+    def apply(self, profile):
+        self.profile = profile
+        plt.rcParams.update(profile.rcParams)
+
+    def add(self, *args, **kwargs):
+        self.__subplots.append(self.SubPlot(*args, **kwargs))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        if isinstance(data, str):
+            self.data.append(pd.read_csv(data, engine='python', header=None))
+        elif isinstance(data, pd.DataFrame) or isinstance(data, np.ndarray):
+            self.data.append(data)
+        else:
+            raise NotImplementedError(type(data) + \
+                                      " is not supported by smartplot")
+
+
+        elif ()
+
         addplot._axes = _plt.subplots()[1]
 
         for count in range(number):
@@ -86,10 +181,7 @@ def addplot(
     if xerr or yerr:
         _plt.errorbar(x, y, xerr=xerr, yerr=yerr)
 
-    # Label text
-    if label:
-        label = r"$K=(" + "{:.3f}".format(s) + r"\pm" + "{:.3f}".format(s_err) + ")$ " + units
-        addplot._axes.text(labelx, labely, label, transform=addplot._axes.transAxes, bbox={'facecolor':'white', 'edgecolor':'black', 'pad':10})
+
 
     # Grid
     addplot._axes.grid(color='#e5e5e5', linestyle='--', linewidth=0.2)
